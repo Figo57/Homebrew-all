6996614fb7d8a461b5fc4bdebec0bb5ecaeb290a
Figo57 <ambmc57@gmail.com>
2023-11-05 10:18:36 +0800

    1.1%

--- Casks/chatgpt-next-web.rb
+++ Casks/chatgpt-next-web.rb
@@ -1,10 +1,10 @@
 cask "chatgpt-next-web" do
-  version "v2.9.9"
-  sha256 "a318b5ad48e4e719da16de7c78c8015f795dbc7cac8ca49397bc2bf587529270" 
-  url "https://github.com/Figo57/Homebrew-all/releases/download/v2.9.9/ChatGPT.Next.Web_2.9.9_aarch64.dmg"
-  name "ChatGPT Next Web.app"
-  desc "Figo57's building Frist ChatGPT Next Web.app"
-  homepage "https://github.com/Figo57/Homebrew-all/Casks/chatgpt-next-web.rb"
+  version "2.9.9"
+  sha256 "a318b5ad48e4e719da16de7c78c8015f795dbc7cac8ca49397bc2bf587529270"
+  url "https://github.com/Figo57/Homebrew-all/releases/download/#{version}/ChatGPT.Next.Web_#{version}_aarch64.dmg"
+  name "ChatGPT Next Web"
+  desc "A web version of ChatGPT by Figo57"
+  homepage "https://github.com/Figo57/Homebrew-all"
   app "ChatGPT Next Web.app"
-
+  
 end
