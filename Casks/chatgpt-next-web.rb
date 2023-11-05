c3070cc64dcb985daaf100e8ba6c09b689f4a90d
Figo57 <ambmc57@gmail.com>
2023-11-06 01:29:41 +0800

    2.5%

--- Casks/chatgpt-next-web.rb
+++ Casks/chatgpt-next-web.rb
@@ -1,10 +1,10 @@
 cask "chatgpt-next-web" do
   version "2.9.9"
   sha256 "a318b5ad48e4e719da16de7c78c8015f795dbc7cac8ca49397bc2bf587529270"
-  url "https://github.com/Figo57/Homebrew-all/releases/download/v2.9.9/ChatGPT.Next.Web_2.9.9_aarch64.dmg"
+  url "https://github.com/Figo57/Homebrew-all/releases/download/2.9.9/ChatGPT.Next.Web_2.9.9_x64.dmg"
   name "ChatGPT Next Web"
   desc "A web version of ChatGPT by Figo57"
-  homepage "https://github.com/Figo57/Homebrew-all"
+  homepage "https://github.com/Figo57/GPT-Assets.git"
   app "ChatGPT Next Web.app"
 
 end
