6616796606f7de9b555d49a1540f181737b3d47b
$USER <ambmc57@gmail.com>
2023-12-30 12:08:05 +0800

    +Gpt4all2

--- Casks/Gpt4all.rb
+++ Casks/Gpt4all.rb
@@ -1,4 +1,4 @@
-cask "gpt4all-installer-darwin" do
+cask "Gpt4all" do
   version "2.5.4"
   sha256 "d28cfde6f483300ef8269f7b6a6ca3de71a92c8a0880d0cd1a9af41ab97e6d21"
   url "https://github.com/Figo57/gpt4all/releases/download/v2.5.4/gpt4all-installer-darwin-v2.5.4.dmg"
