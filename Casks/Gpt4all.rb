2331581b7d00e77fb89a3e9c375f11faa0c4fda3
Figo57 <ambmc57@gmail.com>
2023-12-30 23:04:05 +0800

    Update Gpt4all.rb

--- Casks/Gpt4all.rb
+++ Casks/Gpt4all.rb
@@ -1,10 +1,10 @@
 cask "Gpt4all" do
   version "v2.5.4"
   sha256 "d28cfde6f483300ef8269f7b6a6ca3de71a92c8a0880d0cd1a9af41ab97e6d21"
-  url "https://github.com/Figo57/gpt4all/releases/download/v2.5.4/gpt4all-installer-darwin-v2.5.4.dmg"
+  url "https://github.com/Figo57/gpt4all/releases/download/2.5.4/gpt4all-installer-darwin-v2.5.4.dmg"
   name "Gpt4all"
   desc "GPT4All  is a tool for language generation"
-  homepage "https://github.com/Figo57/gpt4all"
+  homepage "https://github.com/Figo57/gpt4all.git"
   app "gpt4all-installer-darwin.app"
 
 end
