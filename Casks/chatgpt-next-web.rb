b7b4bfd27920cfc4a2137c366d3343b73c7fcf68
$USER <ambmc57@gmail.com>
2024-03-02 23:28:41 +0800

    Update ChatGPT Next Web to version 2.11.2

--- Casks/chatgpt-next-web.rb
+++ Casks/chatgpt-next-web.rb
@@ -1,7 +1,7 @@
 cask "chatgpt-next-web" do
-  version "2.10.3"
-  sha256 "ea1a537f44209d8b7a33e7365f1a5eb6bce249160c7076709490026d8e30dad0"
-  url "https://github.com/Figo57/GPT-Assets/releases/download/2.10.3/NextChat_2.10.3_universal.dmg"
+  version "2.11.2"
+  sha256 "3c43a6db2ec26f13f6929561ba32886f563ca42d2757ba502ab4a7dff7c2f3be"
+  url "https://github.com/Figo57/GPT-Assets/releases/download/2.10.3/NextChat_2.11.2_universal.dmg"
   name "ChatGPT Next Web"
   desc "A web version of ChatGPT by Figo57"
   homepage "https://github.com/Figo57/GPT-Assets.git"
